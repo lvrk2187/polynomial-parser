eded98e66d1d011406d44371ec2698167ac93db0
fractal_edge <adanr.lee@icloud.com>
2026-08-03 19:52:39 +0100

    testing function with vardic macro finished

--- tests/tests.c
+++ tests/tests.c
@@ -17,33 +17,52 @@ void EXPECTED_POLY(polynomial p, int argc, ...) {
   va_list elements;
   va_start(elements, argc);
 
+  int expected_elements_in_c_mono[NUMBER_OF_ELEMENTS_IN_A_MONOMIAL];
+  int actual_elements_in_c_mono[NUMBER_OF_ELEMENTS_IN_A_MONOMIAL];
+
   for (int i = 0; i < (argc >> 2); i++) {
-    // assert();  
+
+    actual_elements_in_c_mono[0] = p.items[i].c_operator;
+    actual_elements_in_c_mono[1] = p.items[i].coefficient;
+    actual_elements_in_c_mono[2] = p.items[i].e_operator;
+    actual_elements_in_c_mono[3] = p.items[i].exponent;
+
+    for (int m = 0; m < NUMBER_OF_ELEMENTS_IN_A_MONOMIAL; m++) expected_elements_in_c_mono[m] = va_arg(elements, int);
+
+     for (int k = 0; k < NUMBER_OF_ELEMENTS_IN_A_MONOMIAL; k++) {
+
+        int actual = actual_elements_in_c_mono[k];
+        int expected = expected_elements_in_c_mono[k];
+
+        if (actual == expected) printf("passed: ");
+        else printf("failed: ");
+
+        printf("expected %d, actual %d\n", expected, actual);
+    }    
   }
 }
 
 void poly_one() {
 
 
-  char p_string[] = "x^2 + 3x^5 - 3x^8 + 2";
+  char p_string[] = "x^2 + 3x^5 - 3x^8 + 2x^0";
   polynomial *p = parser(p_string);
-  EXPECTED_POLY(*p, sizeofpoly(p_string) * (int) (NUMBER_OF_ELEMENTS_IN_A_MONOMIAL), 3);
-  
-  assert(p->items[0].coefficient == 1);
-  assert(p->items[0].exponent == 2); 
-  
+  EXPECTED_POLY (
+                  *p,
+                  sizeofpoly(p_string) * (int) (NUMBER_OF_ELEMENTS_IN_A_MONOMIAL),
+                   1, 1, 1, 2,
+                   1, 3, 1, 5,
+                  -1, 3, 1, 8,
+                   1, 2, 1, 0
+                );
+    
   polynomial_free(p);
 }
 
 int main() {
  
-  // poly_one();
+  poly_one();
   char p_string_1[] = "x^2 + 3x + 2x^0";
 
   char p_string_2[] = "x^2 + 3x^5 - 3x^8 + 2x^0";
-  // polynomial *p = parser(p_string);
-
-  printf("%d\n", sizeofpoly(p_string_1));
-
-  printf("%d", sizeofpoly(p_string_2));
 }
